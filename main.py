01e0fbcfa54c5739b6a5bd90c764de9736061e3e
Huibq <1622881530@qq.com>
2024-04-02 21:36:11 +0800

    up

--- main.py
+++ main.py
@@ -3,40 +3,28 @@ import os
 from utils.util import ae
 from utils.util import ad
 from modules import tx
-from utils.util import CreateObject
 
 key = os.environ['key']
 iv = os.environ['iv']
-createObject = CreateObject
 
 with open("./utils/secret.json", "r", encoding='utf-8') as f:
     txt = json.load(f)
 
-tools = createObject(txt)
-try:
-    tools.User_1.qqmusic_key = ae(tx.refresh(str(ad(tools.User_1.loginuin, key, iv)), ad(tools.User_1.qqmusic_key, key, iv)), key, iv)
-except Exception as e:
-    print(e)
-try:
-    tools.User_2.qqmusic_key = ae(tx.refresh(str(ad(tools.User_2.loginuin, key, iv)), ad(tools.User_2.qqmusic_key, key, iv)), key, iv)
-except Exception as e:
-    print(e)
-try:
-    tools.User_3.qqmusic_key = ae(tx.refresh(str(ad(tools.User_3.loginuin, key, iv)), ad(tools.User_3.qqmusic_key, key, iv)), key, iv)
-except Exception as e:
-    print(e)
-try:
-    tools.User_4.qqmusic_key = ae(tx.refresh(str(ad(tools.User_4.loginuin, key, iv)), ad(tools.User_4.qqmusic_key, key, iv)), key, iv)
-except Exception as e:
-    print(e)
-try:
-    tools.User_5.qqmusic_key = ae(tx.refresh(str(ad(tools.User_5.loginuin, key, iv)), ad(tools.User_5.qqmusic_key, key, iv)), key, iv)
-except Exception as e:
-    print(e)
+
+def doJob(user):
+    try:
+        txt[user]['qqmusic_key'] = ae(tx.refresh(str(ad(txt[user]['loginuin'], key, iv)), ad(txt[user]['qqmusic_key'], key, iv)), key, iv)
+    except Exception as e:
+        print('出错！！')
+
+
+for u in txt:
+    doJob(u)
+
 with open("./utils/secret.json", "w", encoding='utf-8') as f:
-    json.dump(tools, f)
+    json.dump(txt, f)
 
-
-# print(ad(tools.User_1.loginuin, key, iv))
-# print(ad(tools.User_1.qqmusic_key, key, iv))
+# print(ad(tools.User_6.loginuin, key, iv))
+# print(ad(tools.User_6.qqmusic_key, key, iv))
 # print(ae('', key, iv))
+# print(tx.refresh('', ''))
