a90320cba96c5939f44b52f58821e3215464f83e
Huibq <huibq520@gmail.com>
2025-01-07 22:40:47 +0800

    up

--- main2.py
+++ main2.py
@@ -11,12 +11,10 @@ with open("./utils/secret2.json", "r", encoding='utf-8') as f:
 
 
 def doJob(user):
-    if txt[user] != {}:
-        for _user in txt[user]:
-            try:
-                txt[user][_user]['qqmusic_key'] = ae(tx.refresh(str(ad(txt[user][_user]['loginuin'], key, iv)), ad(txt[user][_user]['qqmusic_key'], key, iv)), key, iv)
-            except Exception as e:
-                print(f'{_user}：出错！！')
+    try:
+        txt[user]['qqmusic_key'] = ae(tx.refresh(str(ad(txt[user]['loginuin'], key, iv)), ad(txt[user]['qqmusic_key'], key, iv)), key, iv)
+    except Exception as e:
+        print(f'{user}：出错！！')
 
 
 for u in txt:
@@ -25,7 +23,7 @@ for u in txt:
 with open("./utils/secret2.json", "w", encoding='utf-8') as f:
     json.dump(txt, f)
 
-# print(ad(txt['3m']['User_168']['loginuin'], key, iv))
-# print(ad(txt['3m']['User_168']['qqmusic_key'], key, iv))
+# print(ad(txt['User_186']['loginuin'], key, iv))
+# print(ad(txt['User_186']['qqmusic_key'], key, iv))
 # print(ae('', key, iv))
 # print(ae('', key, iv))
