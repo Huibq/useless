9b2c6af6dec62e44780162b407226d09ab8278be
Huibq <huibq520@gmail.com>
2025-02-24 15:47:18 +0800

    up

--- write_user.py
+++ write_user.py
@@ -7,12 +7,14 @@ iv = os.environ['iv']
 
 uin = input('请输入uin:')
 qqmusickey = input('请输入qqmusickey:')
-# secrte_type = input('请选择写入文档:')
+secrte_type = input('请选择写入文档:')
 secrte_path = './utils/secret.json'
-# if secrte_type == '1':
-#     secrte_path = './utils/secret.json'
-# else:
-#     secrte_path = './utils/secret2.json'
+date = None
+if secrte_type == '1':
+    secrte_path = './utils/secret.json'
+else:
+    secrte_path = './utils/secret2.json'
+    date = input('请输入日期:')
 
 with open(secrte_path, "r", encoding='utf-8') as f:
     txt = json.load(f)
@@ -23,10 +25,12 @@ enc_qqmusickey = ae(qqmusickey, key, iv)
 
 user_name = 'User_{}'.format(len(txt) + 1)
 
+print(user_name)
+
 txt[user_name] = {
         "qqmusic_key": enc_qqmusickey,
         "loginuin": enc_uin,
-        "tab": "qq"
+        "tab": "qq" if date is None else date
     }
 
 with open(secrte_path, "w", encoding='utf-8') as f:
