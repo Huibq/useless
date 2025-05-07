b9d2a6c294a6d0739c4ab670f4311d77648e89f3
Huibq <huibq520@gmail.com>
2025-05-07 21:10:14 +0800

    up

--- utils/Http.py
+++ utils/Http.py
@@ -1,6 +1,12 @@
-import json
+import json, os
 import requests
 from modules.qqsign import sign
+
+socks = os.environ['socks']
+proxies = {
+    'http': socks,
+    'https': socks
+}
 
 
 def signRequest(data):
@@ -9,4 +15,4 @@ def signRequest(data):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0'
     }
-    return requests.post('https://u.y.qq.com/cgi-bin/musics.fcg?format=json&sign=' + s, data=data, headers=headers)
+    return requests.post('https://u.y.qq.com/cgi-bin/musics.fcg?format=json&sign=' + s, data=data, headers=headers, proxies=proxies)
