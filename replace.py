838f32e9ce1ff840694029e48eb64dddcee13ee8
Huibq <huibq520@gmail.com>
2025-10-21 21:52:24 +0800

    更新

---
+++ replace.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+key = os.environ['key']
+iv = os.environ['iv']
+
+User = input('请输入需要替换掉User:')
+secrte_type = input('请选择写入文档:')
+secrte_path = './utils/secret.json'
+date = None
+if secrte_type == '1':
+    secrte_path = './utils/secret.json'
+elif secrte_type == '2':
+    secrte_path = './utils/secret2.json'
+    date = input('请输入日期:')
+elif secrte_type == '3':
+    secrte_path = './utils/secret3.json'
+    date = input('请输入日期:')
+else:
+    secrte_path = './utils/secret.json'
+
+with open(secrte_path, "r", encoding='utf-8') as f:
+    txt = json.load(f)
+    txt[User] = txt[f"User_{len(txt)}"]
+    txt.pop(f"User_{len(txt)}")
+    with open(secrte_path, "w", encoding='utf-8') as f:
+        json.dump(txt, f)
+    print('替换完成！')
