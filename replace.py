eea3bcc9264a14f8487a7079b53985377255bda7
Huibq <huibq520@gmail.com>
2026-02-19 16:54:07 +0800

    up

--- replace.py
+++ replace.py
@@ -1,26 +1,35 @@
 import json
 import os
+from utils.util import ae, ad
 
 key = os.environ['key']
 iv = os.environ['iv']
+while True:
+    User = input('请输入需要替换掉uin:')
+    if User == 'exit':
+        break
+    en_User = ae(User, key, iv)
 
-User = input('请输入需要替换掉User:')
-secrte_type = input('请选择写入文档:')
-secrte_path = './utils/secret.json'
-date = None
-if secrte_type == '1':
-    secrte_path = './utils/secret.json'
-elif secrte_type == '2':
-    secrte_path = './utils/secret2.json'
-elif secrte_type == '3':
-    secrte_path = './utils/secret3.json'
-else:
-    secrte_path = './utils/secret.json'
+    secrte_data = ['./utils/secret.json', './utils/secret2.json', './utils/secret3.json']
+    for secrte_path in secrte_data:
+        with open(secrte_path, "r", encoding='utf-8') as f:
+            txt = json.load(f)
+            Type = False
+            for i in txt:
+                if txt[i]['loginuin'] == en_User:
+                    Type = True
+                    if secrte_path == './utils/secret2.json' or secrte_path == './utils/secret3':
+                        Continue = input('是否继续替换，1是2否:')
+                        if Continue == '2':
+                            break
+                    txt[i] = txt[f"User_{len(txt)}"]
+                    txt.pop(f"User_{len(txt)}")
+                    with open(secrte_path, "w", encoding='utf-8') as F:
+                        json.dump(txt, F)
+                    print('替换完成！')
+                    print('替换路径：{}'.format(secrte_path))
+                    break
+            if Type:
+                Type = False
+                break
 
-with open(secrte_path, "r", encoding='utf-8') as f:
-    txt = json.load(f)
-    txt[User] = txt[f"User_{len(txt)}"]
-    txt.pop(f"User_{len(txt)}")
-    with open(secrte_path, "w", encoding='utf-8') as F:
-        json.dump(txt, F)
-    print('替换完成！')
