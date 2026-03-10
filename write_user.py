af96de0ad764f94592a2c38d19b9f4dc15c078f5
Huibq <huibq520@gmail.com>
2026-03-10 20:16:38 +0800

    up

--- write_user.py
+++ write_user.py
@@ -2,55 +2,60 @@ import json
 import os
 from utils.util import ae
 
-key = os.environ['key']
-iv = os.environ['iv']
+KEY = os.getenv("key")
+IV = os.getenv("iv")
+if not KEY or not IV:
+    raise RuntimeError("缺少环境变量 key 或 iv")
 
-uin = input('请输入uin:')
+FILES = {
+    "1": "./utils/secret.json",
+    "2": "./utils/secret2.json",
+    "3": "./utils/secret3.json"
+}
 
-all_user = []
-for i in range(3):
-    Path = f'./utils/secret.json' if i == 0 else f'./utils/secret{i + 1}.json'
-    with open(Path, "r", encoding='utf-8') as f:
-        users = json.load(f)
-        for user in users:
-            all_user.append(users[user]['loginuin'])
 
-enc_uin = ae(uin, key, iv)
+def load(path):
+    if not os.path.exists(path):
+        return {}
+    try:
+        return json.load(open(path, "r", encoding="utf-8"))
+    except:
+        return {}
 
-if enc_uin in all_user:
+
+def next_user(data):
+    if not data:
+        return "User_1"
+    return f"User_{max(int(k.split('_')[1]) for k in data)+1}"
+
+
+uin = input("请输入uin: ").strip()
+enc_uin = ae(uin, KEY, IV)
+
+all_uin = []
+for p in FILES.values():
+    all_uin += [v["loginuin"] for v in load(p).values()]
+
+if enc_uin in all_uin:
     print(f"已存在：{enc_uin}")
-else:
-    qqmusickey = input('请输入qqmusickey:')
-    secrte_type = input('请选择写入文档:')
-    secrte_path = './utils/secret.json'
-    date = None
-    if secrte_type == '1':
-        secrte_path = './utils/secret.json'
-    elif secrte_type == '2':
-        secrte_path = './utils/secret2.json'
-        date = input('请输入日期:')
-    elif secrte_type == '3':
-        secrte_path = './utils/secret3.json'
-        date = input('请输入日期:')
-    else:
-        secrte_path = './utils/secret.json'
+    exit()
 
-    with open(secrte_path, "r", encoding='utf-8') as f:
-        txt = json.load(f)
+qqmusickey = input("请输入qqmusickey: ").strip()
+t = input("请选择写入文档(1/2/3): ").strip()
 
-        enc_qqmusickey = ae(qqmusickey, key, iv)
+path = FILES.get(t, FILES["1"])
+date = input("请输入日期: ").strip() if t in ("2", "3") else None
 
-        user_name = 'User_{}'.format(len(txt) + 1)
+data = load(path)
+user = next_user(data)
 
-        print(user_name)
+data[user] = {
+    "qqmusic_key": ae(qqmusickey, KEY, IV),
+    "loginuin": enc_uin,
+    "tab": date or "qq"
+}
 
-        txt[user_name] = {
-                "qqmusic_key": enc_qqmusickey,
-                "loginuin": enc_uin,
-                "tab": "qq" if date is None else date
-            }
+json.dump(data, open(path, "w", encoding="utf-8"))
 
-        with open(secrte_path, "w", encoding='utf-8') as f:
-            json.dump(txt, f)
-
-        print('写入完成！')
+print(user)
+print("写入完成！")
